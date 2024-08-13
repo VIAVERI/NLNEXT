fe0062eff9324ee8b7d6bcbb133442fe24e1ce14
Dilsha Thathsarani <dilsha99t@gmail.com>
2024-08-13 11:44:39 +0530

    MInor changes

--- Frontend/src/components/home/hero/Hero.jsx
+++ Frontend/src/components/home/hero/Hero.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from "react"
-import { hero } from "../../../dummyData"
-import "./hero.css"
-import Card from "./Card"
+import React, { useState } from "react";
+import { hero } from "../../../dummyData";
+import "./hero.css";
+import Card from "./Card";
 
 const Hero = () => {
-  const [items, setIems] = useState(hero)
+  const [items, setIems] = useState(hero);
 
   return (
     <>
-      <section className='hero'>
-        <div className='container'>
+      <section className="hero">
+        <div className="container">
           {items.map((item) => {
             return (
               <>
                 <Card key={item.id} item={item} />
               </>
-            )
+            );
           })}
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
