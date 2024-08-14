63ac443981a34a6455537613081bb223c5c0e56e
JayasekaraKD <kavyajayasekara@gmail.com>
2024-08-14 12:09:04 +0530

    popular

--- Frontend/src/components/home/mainContent/popular/Popular.jsx
+++ Frontend/src/components/home/mainContent/popular/Popular.jsx
@@ -1,13 +1,28 @@
-import React from "react"
-import "./Popular.css"
+import React, { useEffect, useState } from "react";
+import "./Popular.css";
 
-import Slider from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
-import { popular } from "../../../../dummyData"
-import Heading from "../../../common/heading/Heading"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Heading from "../../../common/heading/Heading";
 
 const Popular = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/articles"); // Ensure this is the correct API endpoint
+        const data = await response.json();
+        setArticles(data);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
   const settings = {
     className: "center",
     centerMode: false,
@@ -27,45 +42,42 @@ const Popular = () => {
         },
       },
     ],
-  }
+  };
+
   return (
-    <>
-      <section className='popular'>
-        <Heading title='Popular' />
-        <div className='content'>
-          <Slider {...settings}>
-            {popular.map((val) => {
-              return (
-                <div className='items'>
-                  <div className='box shadow'>
-                    <div className='images row'>
-                      <div className='img'>
-                        <img src={val.cover} alt='' />
-                      </div>
-                      <div class='category category1'>
-                        <span>{val.catgeory}</span>
-                      </div>
-                    </div>
-                    <div className='text row'>
-                      <h1 className='title'>{val.title.slice(0, 40)}...</h1>
-                      <div className='date'>
-                        <i class='fas fa-calendar-days'></i>
-                        <label>{val.date}</label>
-                      </div>
-                      <div className='comment'>
-                        <i class='fas fa-comments'></i>
-                        <label>{val.comments}</label>
-                      </div>
-                    </div>
+    <section className="popular">
+      <Heading title="Popular" />
+      <div className="content">
+        <Slider {...settings}>
+          {articles.map((article) => (
+            <div className="items" key={article.article_id}>
+              <div className="box shadow">
+                <div className="images row">
+                  <div className="img">
+                    <img src={article.image_url} alt={article.title} />
+                  </div>
+                  <div className="category category1">
+                    <span>{article.category}</span>
                   </div>
                 </div>
-              )
-            })}
-          </Slider>
-        </div>
-      </section>
-    </>
-  )
-}
+                <div className="text row">
+                  <h1 className="title">{article.title.slice(0, 40)}...</h1>
+                  <div className="date">
+                    <i className="fas fa-calendar-days"></i>
+                    <label>{new Date(article.published_at).toLocaleDateString()}</label>
+                  </div>
+                  <div className="comment">
+                    <i className="fas fa-comments"></i>
+                    <label>{article.rating}</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </section>
+  );
+};
 
-export default Popular
+export default Popular;
