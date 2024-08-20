0d7f47a9836407eb2e9e31159ab0612b6b3011df
Dilsha Thathsarani <dilsha99t@gmail.com>
2024-08-20 13:12:26 +0530

    Update partner routes , fix article page styling ,create user account creation and add swap button

--- Backend/server.js
+++ Backend/server.js
@@ -18,27 +18,19 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Import routes
 const articleRoutes = require("./src/Routes/Articles");
-const partnerProfileRoutes = require("./src/Routes/PartnersProfile");
 const partnerRoutes = require("./src/Routes/Partners");
 const serviceRoutes = require("./src/Routes/Services");
-
 const partnersAccRoutes = require("./src/Routes/partnersAccount");
-
 const emailRoutes = require("./src/Routes/Email");
+const userRoutes = require("./src/Routes/Users");
 
 // Use routes
 app.use("/api/articles", articleRoutes);
-
 app.use("/api", emailRoutes);
 app.use("/api/partners", partnerRoutes);
 app.use("/api/services", serviceRoutes);
 app.use("/api/partners_acc", partnersAccRoutes);
-
-app.use("/api/partners_acc", partnerProfileRoutes);
-
-
-
-
+app.use("/api/users", userRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
